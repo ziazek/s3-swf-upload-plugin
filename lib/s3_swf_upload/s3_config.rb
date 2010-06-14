1f3646441602a4fb0ba19d93abcf8ec6131d97bb
Nathan Colgate <nathan@brandnewbox.com>
2010-06-14 11:17:14 -0700

    Makes sure that the config file gets loaded

--- lib/s3_swf_upload/s3_config.rb
+++ lib/s3_swf_upload/s3_config.rb
@@ -11,16 +11,22 @@ module S3SwfUpload
         file = File.open(filename)
         config = YAML.load(file)[Rails.env]
 
+        if config == nil
+          raise "Could not load config options for #{Rails.env} from #{filename}."
+        end
+
         @@access_key_id     = config['access_key_id']
         @@secret_access_key = config['secret_access_key']
         @@bucket            = config['bucket']
         @@max_file_size     = config['max_file_size']
         @@acl               = config['acl'] || 'private'
-      
+
+        
+        
         unless @@access_key_id && @@secret_access_key && @@bucket
           raise "Please configure your S3 settings in #{filename} before continuing so that S3 SWF Upload can function properly."
         end
-      rescue
+      rescue Errno::ENOENT
          # No config file yet. Not a big deal. Just issue a warning
          puts "WARNING: You are using the S3 SWF Uploader gem, which wants a config file at #{filename}, " +
             "but none could be found. You should try running 'rails generate s3_swf_upload:uploader'"

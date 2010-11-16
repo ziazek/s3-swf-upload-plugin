40e154e6c13773b1c017eec2253a7c409cb6e802
Nathan Colgate <nathan@brandnewbox.com>
2010-11-16 09:54:27 -0800

    Removed rake tasks and thus the dependency on aws/s3

--- lib/s3_swf_upload/railtie.rb
+++ lib/s3_swf_upload/railtie.rb
@@ -7,11 +7,7 @@ module S3SwfUpload
     initializer "s3_swf_upload.load_s3_swf_upload_config" do
       S3SwfUpload::S3Config.load_config
     end
-    
-    rake_tasks do
-      load "s3_swf_upload/railties/tasks/s3_swf_upload.rake"
-    end
-    
+
     generators do
       require "s3_swf_upload/railties/generators/uploader/uploader_generator"
     end
